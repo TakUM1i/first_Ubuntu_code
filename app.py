5aea1c84268b38f25b6f673ed11f33452852c19d
takumi <gtakumi0617@gmail.com>
2026-04-24 10:14:58 +0900

    ログに時刻を表示する機能を追加

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/notify')
 def notify():
     with open("app.log", "a") as f:
+	now =datetime.datetime.now()
         f.write("リクエストを受信しました！\n")
     return "Notification received!\n"
 
